74790d48bb9b75d4a2de5fb5c0e5672c7f44e8ab
minjun._.j <126859900+wjsalswns@users.noreply.github.com>
2024-05-08 18:05:42 +0900

    Add files via upload

---
+++ pack/PackageMain1.java
@@ -0,0 +1,10 @@
+package pack;
+
+import pack.a.User;
+
+public class PackageMain1 {
+    public static void main(String[] args) {
+        Data data = new Data();
+        User user = new User();
+    }
+}
