77b2c37016d7666e34bc0c893f8e053ef8c4ff43
minjun._.j <126859900+wjsalswns@users.noreply.github.com>
2024-05-30 18:04:15 +0900

    Add files via upload

---
+++ nullLabel/after/NullLabel.java
@@ -0,0 +1,17 @@
+package nullLabel.after;
+
+public class NullLabel extends Label{
+    public NullLabel() {
+        super("(none)");
+    }
+
+    @Override
+    public boolean isNull() {
+        return true;
+    }
+
+    @Override
+    public void display() {
+
+    }
+}
---
+++ nullLabel/defore/Label.java
@@ -0,0 +1,18 @@
+package nullLabel.defore;
+
+public class Label {
+    private String label;
+
+    public Label(String label) {
+        this.label = label;
+    }
+
+    public void display(){
+        System.out.println("display : " + label);
+    }
+
+    @Override
+    public String toString() {
+        return "\"" + label + "\"";
+    }
+}
